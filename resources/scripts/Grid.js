f958570e273b341ae3e170394771a3c6cce31e30
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-08-07 01:36:20 +0300

    add up movement

--- resources/scripts/Grid.js
+++ resources/scripts/Grid.js
@@ -18,6 +18,14 @@ export default class Grid {
         })
     }
 
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || [];
+            cellGrid[cell.x][cell.y] = cell
+            return cellGrid;
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null);
     }
@@ -33,11 +41,20 @@ class Cell {
     #x
     #y
     #tile
+    #mergeTile
 
     constructor(cellElement, x, y) {
         this.#cellElement = cellElement;
         this.#x = x;
         this.#y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+
+    get y() {
+        return this.#y;
     }
 
     get tile() {
@@ -49,6 +66,21 @@ class Cell {
         if (value == null) return;
         this.#tile.x = this.#x;
         this.#tile.y = this.#y;
+    }
+
+    get mergeTile() {
+        return this.#mergeTile;
+    }
+
+    set mergeTile(value) {
+        this.mergeTile = value;
+        if (value == null) return;
+        this.#mergeTile.x = this.#x;
+        this.#mergeTile.y = this.#y;
+    }
+
+    canAccept(tile) {
+        return (this.tile == null || (this.tile.mergeTile == null && this.tile.value === tile.value))
     }
 }
 
--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -34,6 +34,30 @@ function handleInput(e) {
 }
 
 function moveUp() {
-    slideTiles()    
+    return slideTiles(grid.cellsByColumn);    
 }
 
+function slideTiles(cells) {
+    cells.forEach(group => {
+        for (let i = 0; i < group; i++) {
+            const cell = group[i];
+            if (cell.tile == null) continue;
+            let lastValidCell;
+            for (let j = i - 1; j >= 0; j--) {
+                const moveToCell = group[j];
+                if (moveToCell.canAccept(cell.tile)) break;
+                lastValidCell = moveToCell;
+            }
+
+            if (lastValidCell != null) {
+                if (lastValidCell.tile != null) {
+                    lastValidCell.mergeTile = cell.tile;
+                } else {
+                    lastValidCell.tile = cell.tile;
+                }
+
+                cell.tile = null;
+            } 
+        }
+    });
+}
