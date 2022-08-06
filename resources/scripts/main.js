143c2f54c36e55eb066736a7b0a5d957f1f54f4a
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-08-07 01:06:16 +0300

    add a keyboard input listener

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -4,7 +4,36 @@ import Tile from "./Tile.js";
 const gameBoard = document.getElementById("gameboard"); 
 
 const grid = new Grid(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput()
 
-grid.randomEmptyCell().tile = new Tile(gameBoard);
-grid.randomEmptyCell().tile = new Tile(gameBoard);
+function setupInput() {
+    window.addEventListener("keydown", handleInput, { once: true });
+}
+
+function handleInput(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        default:
+            setupInput()
+            return;
+    }
+    setupInput()
+}
+
+function moveUp() {
+    slideTiles()    
+}
 
